34c64e057526128b90de547e7b935ead213b0863
Marta Tomzik <marta.tomzik@nxp.com>
2022-12-03 10:53:09 +0100

    unnecessary extra strip

--- 2022/day3/solution.py
+++ 2022/day3/solution.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     with codecs.open("input.txt") as input_file:
         lines = list(map(str.strip, input_file.readlines()))
         calc_priority = lambda x: ord(x) - 96 if ord(x) in range(97, 123) else ord(x) - 38
-        rucksack = list(map(lambda i: (i[:len(i) // 2], i[len(i) // 2:].strip()), lines))
+        rucksack = list(map(lambda i: (i[:len(i) // 2], i[len(i) // 2:]), lines))
         items = list(map(lambda i: set(i[0]).intersection(set(i[1])), rucksack))
         priority_sum = sum(map(lambda i: sum(map(calc_priority, i)), items))
         print("part 1:", priority_sum)
